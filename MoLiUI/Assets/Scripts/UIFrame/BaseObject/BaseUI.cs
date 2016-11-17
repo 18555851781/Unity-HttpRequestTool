1496ea5cfef70ee3fb2423b47daa56e5ed20f4dd
zzjmnfengqing <450282550@qq.com>
2016-11-18 07:55:56 +0800

    BaseUI

---
+++ MoLiUI/Assets/Scripts/UIFrame/BaseObject/BaseUI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace MoLiFrame
+{
+    public class BaseUI : MonoBehaviour 
+    {
+        private GameObject _cacheGameObject;
+        public GameObject CacheGameObject
+        {
+            get
+            {
+                if(null == _cacheGameObject)
+                {
+                    _cacheGameObject = this.gameObject;
+                }
+                return _cacheGameObject;
+            }
+        }
+
+        private Transform _cacheTransform;
+        public Transform CacheTransform
+        {
+            get 
+            { 
+                if(null == _cacheTransform)
+                {
+                    _cacheTransform = this.transform;
+                }
+                return _cacheTransform; 
+            }
+        }
+
+
+        void Start()
+        {
+
+        }
+	
+	   
+	    void Update () 
+        {
+	
+	    }
+    }
+}
---
+++ MoLiUI/Assets/Scripts/UIFrame/Defines/Defines.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+namespace MoLiFrame
+{
+
+    //全局枚举对象
+    public enum EnumObjectState
+    {
+        None,
+        Initial,
+        Loading,
+        Ready,
+        Disabled,
+        Closing
+    }
+
+
+
+    public enum EnumUIType:int
+    {
+        None    = -1,
+        TestOne =  0,
+        TestTwo =  1
+    }
+
+
+
+    public class Defines 
+    {
+       public Defines()
+        {
+
+        }
+    }
+}
